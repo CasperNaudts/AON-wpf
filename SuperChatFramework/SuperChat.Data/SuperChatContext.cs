d9e59c279bb899312748edc39b9d259a9b297b8e
Casper Naudts <casper@edu-tech.be>
2019-05-03 13:33:49 +0200

    start updating connection string to mysql

--- SuperChatFramework/SuperChat.Data/SuperChatContext.cs
+++ SuperChatFramework/SuperChat.Data/SuperChatContext.cs
@@ -10,9 +10,11 @@ namespace SuperChat.Data
         public DbSet<Chat> Chats { get; set; }
         public DbSet<Key> Keys { get; set; }
 
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SuperChatAppData; Trusted_Connection = True; ");
+            //optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SuperChatAppData; Trusted_Connection = True; ");
+            optionsBuilder.UseMySQL("server=cpanel.edu-tech.be; database=casper_securety; userid=casper_edutec1q; pwd=CT{(1vjmC;8&I~#9I7;");
             base.OnConfiguring(optionsBuilder);
         }
     }
